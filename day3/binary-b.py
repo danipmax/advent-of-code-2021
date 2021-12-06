07e7ca1c9417d0913762b4a89b039f2fa184296e
astrochelonian <danpapamax@gmail.com>
2021-12-06 14:51:13 +0000

    part 2 solution with function added

--- day3/binary-b.py
+++ day3/binary-b.py
@@ -15,52 +15,40 @@ else:
     print("The list does not contain elements of the same length, check your input")
     quit()
 
-# find the most common digit and isolate the elements of the input with the most common digit
 
-# find the most common digit for the first bit
-first_bit_list = [string[3] for string in list_str]
-most_common = max(first_bit_list, key=first_bit_list.count)
+def calc_rating(list, rating):
+    """
+    This function calculates the rating (oxygen or co2) using the relevant bit criteria.
 
-# isolate all values that have 1 as first digit
-matching = [s for s in list_str if s[3] == most_common]
+    Parameters:
+        list (list): list of binary values(strings)
+        rating (str): the name rating we are calculating ("oxygen" or "co2")
+    Returns:
+        value (int): decimal value of the rating calculated
+    """
+    rating_list = list
 
-list_str = sorted(list_str)
-oxygen_rating_list = list_str
-co2_rating_list = list_str
-for i in range(number_len):
-    oxygen_rating_first_bit_list = [string[i] for string in oxygen_rating_list]
-    oxygen_rate_count_ones = oxygen_rating_first_bit_list.count("1")
-    oxygen_rate_count_zeros = oxygen_rating_first_bit_list.count("0")
-    oxygen_rating_most_common = max(
-        oxygen_rating_first_bit_list, key=oxygen_rating_first_bit_list.count
-    )
-    if oxygen_rate_count_ones == oxygen_rate_count_zeros:
-        oxygen_rating_list = [s for s in oxygen_rating_list if s[i] == "1"]
-    else:
-        oxygen_rating_list = [
-            s for s in oxygen_rating_list if s[i] == oxygen_rating_most_common
-        ]
+    for i in range(len(list[0])):
+        rating_bit_list = [string[i] for string in rating_list]
+        count_ones = rating_bit_list.count("1")
+        count_zeros = rating_bit_list.count("0")
+        if rating == "oxygen":
+            most_least_common = max(rating_bit_list, key=rating_bit_list.count)
+            tie_value = "1"  # value assigned in case 0 and 1 are equally common
+        elif rating == "co2":
+            most_least_common = min(rating_bit_list, key=rating_bit_list.count)
+            tie_value = "0"  # value assigned in case 0 and 1 are equally common
+        if count_ones == count_zeros:
+            rating_list = [s for s in rating_list if s[i] == tie_value]
+        else:
+            rating_list = [s for s in rating_list if s[i] == most_least_common]
 
-    co2_rating_first_bit_list = [string[i] for string in co2_rating_list]
-    co2_rating_count_ones = co2_rating_first_bit_list.count("1")
-    co2_rating_count_zeros = co2_rating_first_bit_list.count("0")
-    co2_rating_least_common = min(
-        co2_rating_first_bit_list, key=co2_rating_first_bit_list.count
-    )
-    if co2_rating_count_ones == co2_rating_count_zeros:
-        co2_rating_list = [s for s in co2_rating_list if s[i] == "0"]
-    else:
-        co2_rating_list = [
-            s for s in co2_rating_list if s[i] == co2_rating_least_common
-        ]
-    # print(oxygen_rating_list)
-    # print(co2_rating_list)
+    rating = int(rating_list[0], 2)
+    return rating
 
-oxygen_rating = int(oxygen_rating_list[0], 2)
-co2_rating = int(co2_rating_list[0], 2)
 
-# print(oxygen_rating)
-# print(co2_rating)
+oxygen_rating = calc_rating(list_str, "oxygen")
+co2_rating = calc_rating(list_str, "co2")
 
 print(
     f"The life support rating of the submarine is {oxygen_rating * co2_rating} units."
