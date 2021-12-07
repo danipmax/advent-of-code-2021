16ece072c6ec10a8bb94f08323ac799dd3ab6d64
astrochelonian <danpapamax@gmail.com>
2021-12-07 14:39:33 +0000

    interim commit. TODO: convert nested loops to functions

--- day4/squid-a.py
+++ day4/squid-a.py
@@ -10,59 +10,115 @@ raw_input = f.read()
 
 list_str = raw_input.split("\n")
 
-# get a list of the numbers called
+# get the list of the numbers called
 list_numbers_called = [int(i) for i in list_str[0].split(",")]
-list_sets_called = [
-    list_numbers_called[i : i + 5] for i in range(0, len(list_numbers_called), 5)
-]  # i don't know if i need the whole list or if i just need to call them +5 at each iteration
 
-# convert the cards to lists comprising five lists each
+# convert the cards to lists comprising five lists each. Each sublist is a row
 
-list_of_cards = []
+list_of_cards_rows = []
 card = []
 for row in list_str[1:]:
     if row != "":
         row_list = [int(i) for i in row.split()]
         card.append(row_list)
     else:
-        list_of_cards.append(card)
+        list_of_cards_rows.append(card)
         card = []
-list_of_cards.append(card)
+list_of_cards_rows.append(card)
+list_of_cards_rows = [item for item in list_of_cards_rows if item != []]
+# convert the list of cards where the sublists are rows, to a list of cards comrising 5 sublists per card
+# with each sublist representing one column
 
-
-# find winning card
+list_of_cards_cols = []
+for i in range(len(list_of_cards_rows)):
+    card = []
+    for j in range(5):
+        row = list_of_cards_rows[i][j]
+        column = []
+        for m in range(5):
+            col = list_of_cards_rows[i][m][j]
+            column.append(col)
+        card.append(column)
+    list_of_cards_cols.append(card)
 
 # call numbers
-
-
 numbers_called = [
     list_numbers_called[:i] for i in range(0, len(list_numbers_called) + 1, 5)
 ]
+# print(list_of_cards_rows)
+# find winning cards
+# call the numbers
+
+for k in range(len(numbers_called)):
+
+    for i in range(len(list_of_cards_cols)):
+        for j in range(5):
+            # compare each row and column from each card to the numbers called
+            row_to_check = list_of_cards_rows[i][j]
+            col_to_check = list_of_cards_cols[i][j]
+
+            if (all(item in numbers_called[k] for item in row_to_check) == True) or (
+                all(item in numbers_called[k] for item in col_to_check) == True
+            ):
+                print(list_of_cards_rows[i])
+                print(numbers_called[k])
+                print(i)
+                print(k)
+                quit()
+
+# check rows and columns
+
+# for k in range(len(numbers_called)):
+#     for i in range(1, len(list_of_cards)):
+#         for j in range(5):
+#             row = list_of_cards[i][j]
+#             column = []
+#             for m in range(5):
+#                 col = list_of_cards[i][m][j]
+#                 column.append(col)
+
+#             if (all(item in numbers_called[k] for item in row) == True) or (
+#                 all(item in numbers_called[k] for item in column) == True
+#             ):
+#                 print(list_of_cards[i])
+#                 print(numbers_called[k])
+#                 print(i)
+#                 print(k)
+#                 # pick unmarked numbers
+#                 flat_list_card = [item for row in list_of_cards[i] for item in row]
+#                 print(flat_list_card)
+#                 list_unmarked_numbers = [num for num in flat_list_card if num not in numbers_called[k]]
+#                 quit()
+#             else:
+#                 continue
+list_of_cards = list_of_cards_rows
+print("nd")
 
 # check rows
-for k in range(len(numbers_called)):
-    for i in range(1, len(list_of_cards)):
-        for j in range(5):
-            row = list_of_cards[i][j]
-            if all(item in numbers_called[k] for item in row) == True:
-                print(list_of_cards[i])
-                print(numbers_called[k])
-                print(row)
-                # quit()
+# for k in range(len(numbers_called)):
+#     for i in range(1, len(list_of_cards)):
+#         for j in range(5):
+#             row = list_of_cards[i][j]
+#             if all(item in numbers_called[k] for item in row) == True:
+#                 print(list_of_cards[i])
+#                 print(numbers_called[k])
+#                 print(row)
+#                 print(i)
+#                 quit()
 
-    # check columns
-for k in range(len(numbers_called)):
+# check columns
+# for k in range(len(numbers_called)):
 
-    for i in range(1, len(list_of_cards)):
-        for j in range(5):
-            column = []
+#     for i in range(1, len(list_of_cards)):
+#         for j in range(5):
+#             column = []
 
-            for m in range(5):
-                col = list_of_cards[i][m][j]
-                column.append(col)
-            if all(item in numbers_called[k] for item in column) == True:
-                print(list_of_cards[i])
-                print(numbers_called[k])
-                print(column)
-                print(i)
-                quit()
+#             for m in range(5):
+#                 col = list_of_cards[i][m][j]
+#                 column.append(col)
+#             if all(item in numbers_called[k] for item in column) == True:
+#                 print(list_of_cards[i])
+#                 print(numbers_called[k])
+#                 print(column)
+#                 print(i)
+#                 quit()
